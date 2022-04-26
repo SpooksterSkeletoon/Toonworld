e3478492dc95c8a3094f4e22aa220dd884042427
Viktoras Navidauskas <viktoras@nordcode.lt>
2022-04-26 12:50:52 +0300

    Updates

--- src/scripts/Helper.js
+++
@@ -1,6 +0,0 @@
-export const fetchMetadata = async (url) => {
-    const response = await fetch(url);
-    const jsonData = await response.json();
-    //console.log(jsonData)
-    return jsonData;
-  };
